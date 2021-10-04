7327cabc68fe4936e105eb59e51e6f3cae9d8645
Pedro Basilio <pedronobasi@gmail.com>
2021-10-04 16:36:49 -0300

    fixing bugs

--- script.js
+++ script.js
@@ -1,45 +1,3 @@
-// Inti
-let currentPlayer = 1;
-
-fieldSection.addEventListener("click", (evt) => {
-  const selectedColumn = evt.currentTarget;
-
-  if (currentPlayer === 1) {
-    if (addBall === false) {
-      const discA = document.createElement("div");
-      discA.classList.add("discA-style");
-      selectedColumn.appendChild(discA);
-      // chamar funções de checar vitória e empate
-      currentPlayer = 2;
-    }
-  }
-
-  if (currentPlayer === 2) {
-    if (addBall === false) {
-      const discB = document.createElement("div");
-      discB.classList.add("discB-style");
-      selectedColumn.appendChild(discB);
-      // chamar funções de checar vitória e empate
-      currentPlayer = 1;
-    }
-  }
-});
-
-// inti - final
-
-// Pedro
-
-function addBall(){
-    for(let i = 6; i >= 0; i --){
-        if(column.children[i].childElementCount === 0){
-            return true
-        }
-    }
-    return false
-}
-
-
-
 //Sid
 
 
@@ -51,6 +9,7 @@ function createField() {
     for (let i = 0; i < 7; i++) {
         const createColumn = document.createElement('section')
         createColumn.classList = 'column'
+        createColumn.id = `${i}`
         fieldSection.appendChild(createColumn)
         for (let j = 0; j < 6; j++) {
             const createQuad = document.createElement('div')
@@ -61,3 +20,61 @@ function createField() {
     }
 }
 createField()
+
+// Pedro
+const column00 = document.getElementById("0")
+const column01 = document.getElementById("1")
+const column02 = document.getElementById("2")
+const column03 = document.getElementById("3")
+const column04 = document.getElementById("4")
+const column05 = document.getElementById("5")
+const column06 = document.getElementById("6")
+
+function addBall(){
+    for(let i = 5; i >= 0; i --){
+        if(column00.children[i].childElementCount === 0){
+            return true
+        }
+    }
+    return false
+}
+
+// Inti
+
+
+
+function createBall(){    
+let currentPlayer = 1;
+
+column00.addEventListener("click", (evt) => {
+  const selectedColumn = evt.currentTarget;
+
+  console.log("oi")
+  if (currentPlayer === 1) {
+    if (addBall()) {
+      const discA = document.createElement("div");
+      discA.classList.add("discA-style");
+      selectedColumn.appendChild(discA);
+      // chamar funções de checar vitória e empate
+      currentPlayer = 2;
+    }
+  }
+
+  if (currentPlayer === 2) {
+    if (addBall()) {
+      const discB = document.createElement("div");
+      discB.classList.add("discB-style");
+      selectedColumn.appendChild(discB);
+      // chamar funções de checar vitória e empate
+      currentPlayer = 1;
+    }
+  }
+});
+}
+createBall()
+
+// inti - final
+
+
+
+
