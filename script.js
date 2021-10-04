3f80fcab1c0d4de912d30ead4e8503b03774f028
inti ferreira <inti.lima@gmail.com>
2021-10-04 15:32:23 -0300

    feat: insert discs functionality

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 // Inti
 
+/*
+1 - adicionar um eventlistener de click na section que foi clicada no tabuleiro
+2 - capturar o currenttarget do evt e colocar na 
 
 
+*/
+const discA = document.createElement("div");
+const discB = document.createElement("div");
+discA.classList.add("discA-style");
+discB.classList.add("discB-style");
 
+fieldSection.addEventListener("click", (evt) => {
+  const selectedColumn = evt.currentTarget;
+  let currentPlayer = 1;
 
+  if (currentPlayer === 1) {
+    if (addBall === false) {
+      selectedColumn.appendChild(discA);
+      currentPlayer = 2;
+    }
+  }
 
+  if (currentPlayer === 2) {
+    if (addBall === false) {
+      selectedColumn.appendChild(discB);
+      currentPlayer = 1;
+    }
+  }
+});
 
-
+// inti - final
 
 // Pedro
-
-
-
-
-
-
 
 //Sid
