0fc512ce8177f6cee3358cf4e6bf40b6b9d751b9
sidny <sidny.silva@outlook.com>
2021-10-04 14:25:25 -0300

    Updated field creation

--- script.js
+++ script.js
@@ -18,15 +18,19 @@
 
 //Sid
 
+
 function createField() {
+    const fieldSection = document.createElement('section')
+    fieldSection.id = 'field'
     const body = document.getElementById('body')
-    for (let i = 0; i < 6; i++) {
+    body.appendChild(fieldSection)
+    for (let i = 0; i < 7; i++) {
         const createColumn = document.createElement('section')
         createColumn.classList = 'column'
-        body.appendChild(createColumn)
-        for (let j = 0; j < 7; j++) {
+        fieldSection.appendChild(createColumn)
+        for (let j = 0; j < 6; j++) {
             const createQuad = document.createElement('div')
-            createQuad.classList = 'field'
+            createQuad.classList = 'cell'
             createQuad.dataset.pos = `${i}-${j}`
             createColumn.appendChild(createQuad)
         }
