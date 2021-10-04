a6db1412c0be524f001bfa062ba4a149783cf5ac
inti ferreira <inti.lima@gmail.com>
2021-10-04 15:51:56 -0300

    fix: variables order changed and fixed

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // Inti
-const discA = document.createElement("div");
-const discB = document.createElement("div");
-discA.classList.add("discA-style");
-discB.classList.add("discB-style");
+let currentPlayer = 1;
 
 fieldSection.addEventListener("click", (evt) => {
   const selectedColumn = evt.currentTarget;
-  let currentPlayer = 1;
 
   if (currentPlayer === 1) {
     if (addBall === false) {
+      const discA = document.createElement("div");
+      discA.classList.add("discA-style");
       selectedColumn.appendChild(discA);
-      // chamar funções de checar vitória
+      // chamar funções de checar vitória e empate
       currentPlayer = 2;
     }
   }
 
   if (currentPlayer === 2) {
     if (addBall === false) {
+      const discB = document.createElement("div");
+      discB.classList.add("discB-style");
       selectedColumn.appendChild(discB);
-      // chamar funções de checar vitória
+      // chamar funções de checar vitória e empate
       currentPlayer = 1;
     }
   }
