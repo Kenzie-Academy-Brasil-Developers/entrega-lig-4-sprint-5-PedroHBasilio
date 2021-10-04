3ec7cad4921e792e107a128cce7cd20714511a54
sidny <sidny.silva@outlook.com>
2021-10-04 12:34:54 -0300

    Geracao de campo

--- script.js
+++ script.js
@@ -17,3 +17,19 @@
 
 
 //Sid
+
+function createField() {
+    const body = document.getElementById('body')
+    for (let i = 0; i < 6; i++) {
+        const createColumn = document.createElement('section')
+        createColumn.classList = 'column'
+        body.appendChild(createColumn)
+        for (let j = 0; j < 7; j++) {
+            const createQuad = document.createElement('div')
+            createQuad.classList = 'field'
+            createQuad.dataset.pos = `${i}-${j}`
+            createColumn.appendChild(createQuad)
+        }
+    }
+}
+createField()
